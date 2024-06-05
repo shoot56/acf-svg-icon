e4169e45e1bd769b284459dc0ab90204c8a3d69e
Enis <xmarks.dev@gmail.com>
2024-06-05 09:21:52 +0200

    1.0.5 - Patch Fix - Fatal Error Illegal Offset on php 8.2

--- fields/class-swp-acf-svg-icon-v5.php
+++ fields/class-swp-acf-svg-icon-v5.php
@@ -142,7 +142,6 @@ if ( ! class_exists( 'swp_acf_field_svg_icon' ) )  {
                     }
                 }
             } else {
-                // Fatal Error Illegal Offset if $value is empty array
                 if (is_string($value)) {
                     $value = $field['choices'][$value] ?? array();
                 }
